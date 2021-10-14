3be0806163a3a75bc5b71b8768e51cd9c2c88d99
gineff <canone@inbox.ru>
2021-10-14 19:33:47 +0300

    2s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import { useRealmApp, RealmAppProvider } from "./RealmApp";
+
+//import * as Realm from "realm-web";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {Nav, NavDropdown, Container} from 'react-bootstrap';
+import Books from "./Components/Books";
+import Breadcrumb from "./Components/Breadcrumb";
+import Login from "./Components/Login";
+import SignUp from "./Components/Signup";
+import Navbar from "./Components/Navbar";
+import counter from "./Components/counter";
+import Video from "./Components/Video";
 
 function App() {
+
+  const [state, setState] = useState({
+    loading: true,
+    showModal: false,
+    fetching: true,
+    book:{}
+  });
+
+  const appId = "application-0-rgont";
+  //const app = new Realm.App({ id: appId });
+
+  //const [realm, setRealm] = React.useState(new Realm.App(appId));
+  //const [currentUser, setCurrentUser] = useState(realm.currentUser);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RealmAppProvider appId={appId}>
+        <Router>
+          <Navbar/>
+          <div>
+            <Breadcrumb state={state} setState={setState}> </Breadcrumb>
+            <Switch>
+              <Route exact path='/Login' component={Login} />
+              <Route exact path='/counter' component={counter} />
+              <Route exact path='/SignUp' component={SignUp} />
+              <Route exact path='/video' component={Video} />
+              <Route path='/lib/:libName/(author|sequence|genre|list|search)?/:id?'  render={(props)=>(<Books {...props}  state={state} setState={setState}/> )}/>
+              <Route exact path='/'  render={(props)=>(<div><p>Описание сервиса. Выбор библиотеки</p>
+                <Link to={"/lib/flibusta"}>Flibusta.net</Link>
+              </div>)}/>
+            </Switch>
+          </div>
+        </Router>
+      </RealmAppProvider>
     </div>
   );
 }
